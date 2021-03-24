78d4cae8a753c2a5706bd7a4330fc2bae69ad3a4
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-03-24 16:18:24 +0200

    Add toggle for button

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    var isLightOn = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
     @IBAction func buttonPressed() {
+        isLightOn.toggle()
+        view.backgroundColor = isLightOn ? .white : .black
     }
     
 }
